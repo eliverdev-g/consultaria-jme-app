116dc2e3676aea4a3808aa8af2122540f0630bae
eliver <eliver39g@gmail.com>
2026-08-08 11:16:05 -0600

    new

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/consultaria-jme-app/',
-    // ESTO ES CLAVE: Cambia '_nuxt' por 'assets' para que GitHub Pages cargue tus CSS y JS
-    buildAssetsDir: 'assets'
-  },
-
-  nitro: {
-    preset: 'github-pages'
-  },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
@@ -19,12 +9,6 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@vueuse/nuxt'
   ],
-
-  image: {
-    provider: 'none',
-    // ESTO ES CLAVE: Le dice a NuxtImg que siempre agregue tu repositorio al inicio de la URL
-    baseURL: '/consultaria-jme-app/'
-  },
 
   vite: {
     plugins: [
