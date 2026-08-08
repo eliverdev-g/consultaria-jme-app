748b80bb6e1237b7e82ab25715e967ed71b836b3
eliver <eliver39g@gmail.com>
2026-08-08 00:21:00 -0600

    MOD: mejoras para limpiar codigo inecesario

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import AppHeader from '~/components/layouts/AppHeader.vue'
 import HeroSection from '~/components/home/HeroSection.vue'
-import MissionVisionSection from '~/components/home/MissionVisionSection.vue';
+import MissionVisionSection from '~/components/home/MissionVisionSection.vue'
 </script>
 
 <template>
-  <div>
-    <AppHeader />
+  <AppHeader />
 
-    <main>
-      <HeroSection />
-      <MissionVisionSection />
-    </main>
-
-    <!-- <AppFooter /> -->
-  </div>
+  <main>
+    <HeroSection />
+    <MissionVisionSection />
+  </main>
 </template>
